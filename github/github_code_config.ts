cae816bf054062c5c2c4754f4779fae3ae55ced3
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-10-08 14:36:03 -0700

    Create github_code_config.ts

---
+++ github/github_code_config.ts
@@ -0,0 +1,4 @@
+["if_user_content"]
+  github.ccl token .config {'config_source=https://www.gitlab.colab.com/As90909w/code-for-ccl-cloud/vb2/index.cmake'}
+received from ["https://www.gitlab.com/USER_API_CONFIG_TOKEN/As90909w/#8882iwjf83ff"]
+config to ["https://www.github.com/project/new/API_KEYS/#88110"]
